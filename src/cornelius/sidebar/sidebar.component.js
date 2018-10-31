1a94609be16867fae97ead9b08c7385a5f623bdb
Rémy MEJA <remymeja@free.fr>
2018-10-31 08:48:31 +0100

    fix component

--- src/cornelius/sidebar/sidebar.component.js
+++ src/cornelius/sidebar/sidebar.component.js
@@ -2,9 +2,9 @@ import './sidebar.scss';
 import template from './sidebar.template.html';
 
 export const sidebar = {
-  controller: function ($scope) {
-    $scope.apply = function () {
-      console.log(this.$ctrl);
+  controller: function () {
+    this.apply = function () {
+      console.log(this.source, this.score);
     };
     this.$onInit = function () {
       this.score = 90;
