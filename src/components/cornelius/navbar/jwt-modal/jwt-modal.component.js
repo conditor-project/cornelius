338d868f64cfc35dc4993ff8dc991f15de796ada
Rémy MEJA <remymeja@free.fr>
2018-11-06 11:38:05 +0100

    update jwt modal and service

--- src/components/cornelius/navbar/jwt-modal/jwt-modal.component.js
+++ src/components/cornelius/navbar/jwt-modal/jwt-modal.component.js
@@ -1,18 +1,41 @@
+import './jwt-modal.scss';
 import template from './jwt-modal.template.html';
 
 export const jwtModal = {
-  controller: function ($window, jwtService) {
+  controller: function ($window, $http, jwtService) {
     this.$onInit = function () {
-      this.tokenJwt = $window.localStorage.getItem('conditor');
+      this.loading = false;
+      this.tokenJwt = jwtService.getTokenJwt();
     };
 
     this.ok = function () {
-      jwtService.addToken(this.tokenJwt);
-      this.modalInstance.close(this.tokenJwt);
+      this.loading = true;
+      $http.defaults.headers.common.Authorization = 'Bearer ' + this.tokenJwt;
+      $http.get('https://api-dev.conditor.fr/v1/records')
+        .then(() => {
+          this.loading = false;
+          jwtService.saveTokenJwt(this.tokenJwt);
+          this.modalInstance.close();
+        })
+        .catch(response => {
+          this.loading = false;
+          if (String(response.status)[0] === '5') {
+            this.msgErrors = {
+              level: 'danger',
+              text: 'Error server side'
+            };
+          }
+          if (String(response.status)[0] === '4') {
+            this.msgErrors = {
+              level: 'warning',
+              text: 'Token JWT invalid'
+            };
+          }
+        });
     };
 
     this.cancel = function () {
-      this.modalInstance.dismiss('cancel');
+      this.modalInstance.close();
     };
   },
   bindings: {
--- src/services/jwt.service.js
+++ src/services/jwt.service.js
@@ -1,10 +1,11 @@
 export function jwtService ($http, $window) {
-  const jwtService = {};
-  jwtService.addToken = addToken;
+  const jwtService = {
+    saveTokenJwt: function (token) {
+      $window.localStorage.setItem('api-conditor', token);
+    },
+    getTokenJwt: function () {
+      return $window.localStorage.getItem('api-conditor');
+    }
+  };
   return jwtService;
-
-  function addToken (token) {
-    $window.localStorage.setItem('conditor', token);
-    $http.defaults.headers.common.Authorization = 'Bearer ' + token;
-  }
 }
