baacb6faf9f230193430b044f6fcd5ee05d3f7b9
Rémy MEJA <remymeja@free.fr>
2018-11-06 08:24:18 +0100

    update jwt service

--- src/services/jwt.service.js
+++ src/services/jwt.service.js
@@ -1,10 +1,10 @@
-export function jwtService ($http, $localStorage) {
+export function jwtService ($http, $window) {
   const jwtService = {};
   jwtService.addToken = addToken;
   return jwtService;
 
   function addToken (token) {
-    $localStorage.conditor = { token };
+    $window.localStorage.setItem('conditor', token);
     $http.defaults.headers.common.Authorization = 'Bearer ' + token;
   }
 }
