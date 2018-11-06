23477427db9ee95fe7793e0b38ecf7fe0d4ebc4c
Rémy MEJA <remymeja@free.fr>
2018-11-06 13:50:37 +0100

    rename function

--- src/components/cornelius/navbar/navbar.component.js
+++ src/components/cornelius/navbar/navbar.component.js
@@ -2,7 +2,7 @@ import template from './navbar.template.html';
 
 export const navbar = {
   controller: function ($uibModal) {
-    this.open = function () {
+    this.openJwtModal = function () {
       $uibModal.open({
         component: 'jwtModal',
         backdrop: 'static'
