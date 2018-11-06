5811db3a05a22e565067cf8415db2e749df72b19
Rémy MEJA <remymeja@free.fr>
2018-11-06 15:37:19 +0100

    connecting to api-conditor

--- src/components/cornelius/item-list/item-list.component.js
+++ src/components/cornelius/item-list/item-list.component.js
@@ -1,16 +1,23 @@
 import './item-list.scss';
 import template from './item-list.template.html';
-import records from './records.json';
 
 export const itemList = {
-  controller: function () {
+  controller: function ($http, jwtService, API_CONDITOR_CONFIG) {
+    $http.defaults.headers.common.Authorization = 'Bearer ' + jwtService.getTokenJwt();
     this.$onInit = function () {
-      this.items = records;
+      $http.get(API_CONDITOR_CONFIG.baseUrl + '/?exclude=teiBlob')
+        .then((response) => {
+          this.items = response.data;
+        });
     };
 
     this.$onChanges = function () {
-      const sources = Object.keys(this.filterOptions.source).filter(source => this.filterOptions.source[source]);
-      if (sources.length > 0) this.items = records.filter(item => sources.includes(item.source));
+      const sources = Object.keys(this.filterOptions.source).filter(source => this.filterOptions.source[source]).join(' OR ');
+      const requestUrl = API_CONDITOR_CONFIG.baseUrl + `/?q=source:(${sources})&exclude=teiBlob`;
+      $http.get(requestUrl)
+        .then((response) => {
+          this.items = response.data;
+        });
     };
   },
   bindings: {
