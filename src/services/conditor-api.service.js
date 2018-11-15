62dd5a38a0e168c7fc9185288723f695912cf242
Rémy MEJA <remymeja@free.fr>
2018-11-15 08:04:53 +0100

    fix bad request response and adding function

--- src/services/conditor-api.service.js
+++ src/services/conditor-api.service.js
@@ -13,9 +13,16 @@ export function conditorApiService ($http, jwtService, API_CONDITOR_CONFIG) {
       const tokenJwt = jwtService.getTokenJwt();
       if (tokenJwt) $http.defaults.headers.common.Authorization = `Bearer ${tokenJwt}`;
       const sources = Object.keys(filterOptions.source).filter(source => filterOptions.source[source]).join(' OR ');
-      let requestUrl = API_CONDITOR_CONFIG.baseUrl + `/?q=isDuplicate:false AND isNearDuplicate:true AND source:(${sources})`;
+      let requestUrl = API_CONDITOR_CONFIG.baseUrl + '/?q=isDuplicate:false AND isNearDuplicate:true';
+      if (sources.length > 0) requestUrl += ` AND source:(${sources})`;
       if (filterOptions.typeConditor !== 'All') requestUrl += ` AND typeConditor:${filterOptions.typeConditor}`;
       requestUrl += '&exclude=teiBlob';
+      return $http.get(requestUrl);
+    },
+    getAggregationsSource: function () {
+      const tokenJwt = jwtService.getTokenJwt();
+      if (tokenJwt) $http.defaults.headers.common.Authorization = `Bearer ${tokenJwt}`;
+      const requestUrl = API_CONDITOR_CONFIG.baseUrl + `/?aggs=terms:source&size=0`;
       return $http.get(requestUrl);
     }
   };
