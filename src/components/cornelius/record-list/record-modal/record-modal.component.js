eb047c758b57939428710e8cd6067e1cf98518dc
Rémy MEJA <remymeja@free.fr>
2018-11-26 16:19:28 +0100

    deal with the undefined value and better filter field

--- src/components/cornelius/record-list/record-modal/record-modal.component.js
+++ src/components/cornelius/record-list/record-modal/record-modal.component.js
@@ -1,10 +1,9 @@
 import './record-modal.scss';
 import template from './record-modal.template.html';
 import { diffWords } from 'diff';
-import { mappings } from 'co-config/mapping.json';
 
 export const recordModal = {
-  controller: function ($q, $http, jwtService, API_CONDITOR_CONFIG) {
+  controller: function ($q, conditorApiService) {
     this.$onInit = function () {
       this.record = this.resolve.record;
       this.recordsComparison = {};
@@ -27,25 +26,18 @@ export const recordModal = {
 
     this.getComparisonInfos = function () {
       Object.keys(this.record).map(key => {
-        const fieldsToIgnore = Object.keys(mappings.record.properties)
-          .map(key => {
-            mappings.record.properties[key].name = key;
-            return mappings.record.properties[key];
-          })
-          .filter(property => (property.type === 'nested') || property.type === 'boolean')
-          .filter(property => property.name !== 'nearDuplicate')
-          .map(property => property.name)
-          ;
-        fieldsToIgnore.push('path', 'nearDuplicate', 'ingestId', 'creationDate', 'score', 'idChain', 'idConditor');
-        if (fieldsToIgnore.includes(key)) return;
+        const notDisplay = ['_score', 'idConditor', 'nearDuplicate'].includes(key);
+        if (notDisplay) return;
         let record = this.record[key];
-        let nearDuplicateRecordSelected = this.nearDuplicateRecordSelected[key];
+        let nearDuplicateRecordSelected = this.nearDuplicateRecordSelected.hasOwnProperty(key) ? this.nearDuplicateRecordSelected[key] : '';
         record = (typeof record === 'string') ? record : String(record);
         nearDuplicateRecordSelected = (typeof nearDuplicateRecord === 'string') ? nearDuplicateRecordSelected : String(nearDuplicateRecordSelected);
         if ((record.length + nearDuplicateRecordSelected.length) === 0) return;
         const isEqual = (record.toLowerCase() === nearDuplicateRecordSelected.toLowerCase());
         const averageNumberCharacters = (record.length + nearDuplicateRecordSelected.length) / 2;
-        if (averageNumberCharacters < 3000) {
+        const maxSizeAbstract = 3000;
+        const toCompare = !['source'].includes(key);
+        if (averageNumberCharacters < maxSizeAbstract && toCompare) {
           const comparison = diffWords(record, nearDuplicateRecordSelected, { ignoreCase: true });
           const origin = comparison.filter(chunk => (!chunk.added));
           const target = comparison.filter(chunk => (!chunk.removed));
@@ -63,17 +55,14 @@ export const recordModal = {
     };
 
     this.getNearDuplicate = function () {
-      $http.defaults.headers.common.Authorization = 'Bearer ' + jwtService.getTokenJwt();
       const nearDuplicateRecords = this.record.nearDuplicate.map(nearDuplicateRecord => {
-        const requestUrl = API_CONDITOR_CONFIG.baseUrl + '/' + String(nearDuplicateRecord.idConditor) + '?exclude=teiBlob';
-        return $http.get(requestUrl);
+        return conditorApiService.getRecordById(nearDuplicateRecord.idConditor);
       });
       return $q.all(nearDuplicateRecords).then(responses => {
-        this.nearDuplicateRecords = responses
-          .map(response => {
-            response.data.isSelected = false;
-            return response.data;
-          });
+        this.nearDuplicateRecords = responses.map(response => {
+          response.data.isSelected = false;
+          return response.data;
+        });
       });
     };
   },
