95cae1420684415eb967b34c17bc5e30ab9760e5
Rémy MEJA <remymeja@free.fr>
2018-12-12 10:18:38 +0100

    update jwt-modal service

--- src/components/cornelius/jwt-modal/jwt-modal.service.js
+++ src/components/cornelius/jwt-modal/jwt-modal.service.js
@@ -1,13 +1,24 @@
 export function jwtModalService ($uibModal) {
+  let isOpen = false;
+  let modalInstance;
   return {
     open: function (options = { force: false }) {
-      return $uibModal.open({
+      if (isOpen) return modalInstance;
+      const modal = $uibModal.open({
         component: 'jwtModal',
         backdrop: 'static',
         resolve: {
           options: () => options
         }
       }).result;
+
+      isOpen = true;
+      modalInstance = modal;
+
+      modal.finally(() => {
+        isOpen = false;
+      });
+      return modal;
     }
   };
 }
