300bc44b48739d1dfde20918efb9046df57a5c84
Rémy MEJA <remymeja@free.fr>
2019-01-14 09:17:10 +0100

    update component

--- src/components/cornelius/sort/sort.component.js
+++ src/components/cornelius/sort/sort.component.js
@@ -1,11 +1,16 @@
 import './sort.scss';
 import template from './sort.template.html';
+import angular from 'angular';
 
 export const sort = {
   controller: function () {
     this.$onInit = function () {
       this.options = ['titre (A-Z)', 'titre (Z-A)'];
       this.optionSelected = this.options[0];
+    };
+    this.onChange = function () {
+      const newOptions = angular.copy(this.optionSelected);
+      this.onOptionsChange({ newOptions });
     };
   },
   bindings: {
