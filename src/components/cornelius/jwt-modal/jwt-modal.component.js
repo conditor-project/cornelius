5dbb48e8e6fed67b6190162b698d6a6873f37eac
Rémy MEJA <remymeja@free.fr>
2019-01-17 08:27:02 +0100

    add jwt-decode for expired token detection

--- src/components/cornelius/jwt-modal/jwt-modal.component.js
+++ src/components/cornelius/jwt-modal/jwt-modal.component.js
@@ -1,5 +1,6 @@
 import './jwt-modal.scss';
 import template from './jwt-modal.template.html';
+import jwtDecode from 'jwt-decode';
 
 export const jwtModal = {
   controller: function ($http, jwtService, API_CONDITOR_CONFIG) {
@@ -10,30 +11,41 @@ export const jwtModal = {
     };
 
     this.save = function () {
-      this.loading = true;
-      $http.defaults.headers.common.Authorization = 'Bearer ' + this.tokenJwt;
-      $http.get(`${API_CONDITOR_CONFIG.baseUrl}/${API_CONDITOR_CONFIG.routes.record}`)
-        .then(() => {
-          this.loading = false;
-          jwtService.saveTokenJwt(this.tokenJwt);
-          this.modalInstance.close();
-        })
-        .catch(response => {
-          this.loading = false;
-          $http.defaults.headers.common.Authorization = '';
-          if (String(response.status)[0] === '5') {
-            this.msgErrors = {
-              level: 'danger',
-              text: 'Error server side'
-            };
-          }
-          if (String(response.status)[0] === '4') {
-            this.msgErrors = {
-              level: 'warning',
-              text: 'Invalid or expired token JWT'
-            };
-          }
-        });
+      const decodedTokenJwt = jwtDecode(this.tokenJwt);
+      const expired = new Date(decodedTokenJwt.exp * 1000);
+      const now = Date.now();
+      const isTokenExpired = expired < now;
+      if (isTokenExpired) {
+        this.msgErrors = {
+          level: 'warning',
+          text: `Le jeton JWT a expiré le ${expired.toLocaleString('fr-FR', { timeZone: 'UTC' })}`
+        };
+      } else {
+        this.loading = true;
+        $http.defaults.headers.common.Authorization = 'Bearer ' + this.tokenJwt;
+        $http.get(`${API_CONDITOR_CONFIG.baseUrl}/${API_CONDITOR_CONFIG.routes.record}`)
+          .then(() => {
+            this.loading = false;
+            jwtService.saveTokenJwt(this.tokenJwt);
+            this.modalInstance.close();
+          })
+          .catch(response => {
+            this.loading = false;
+            $http.defaults.headers.common.Authorization = '';
+            if (String(response.status)[0] === '5') {
+              this.msgErrors = {
+                level: 'danger',
+                text: 'Error server side'
+              };
+            }
+            if (String(response.status)[0] === '4') {
+              this.msgErrors = {
+                level: 'warning',
+                text: 'Le jeton JWT est invalide'
+              };
+            }
+          });
+      }
     };
 
     this.cancel = function () {
