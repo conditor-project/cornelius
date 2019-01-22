c503cb2142123dd2197b9080e226ba4141a31182
Rémy MEJA <remymeja@free.fr>
2019-01-22 14:05:40 +0100

    fix percentage filter

--- src/app.js
+++ src/app.js
@@ -64,7 +64,7 @@ angular
   .directive('myEnterKeypress', myEnterKeypress)
   .filter('percentage', ['$filter', function ($filter) {
     return function (input, decimals = 2) {
-      return $filter('number')(input * 100, decimals) + '%';
+      return $filter('number')(input * 100, decimals) + ' %';
     };
   }])
 ;
