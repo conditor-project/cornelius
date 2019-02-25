d5a28cd10e17336220a7e0eb5eb2a9c64eab04c7
Rémy MEJA <remymeja@free.fr>
2019-02-25 10:40:41 +0100

    code refactoring

--- src/components/cornelius/record-list/record-modal/record-modal.component.js
+++ src/components/cornelius/record-list/record-modal/record-modal.component.js
@@ -12,13 +12,23 @@ export const recordModal = {
       this.sizeColumnHeaderRecord = 6;
       this.sizeColumnHeaderNested = 6;
       this.hasNearDuplicateRecordsToValidate = false;
-      this.getNearDuplicate().then(() => {
+      getNearDuplicates(this.record, conditorApiService).then(nearDuplicates => {
+        this.nearDuplicateRecords = nearDuplicates.map(response => {
+          const output = angular.copy(response.data);
+          output.isSelected = false;
+          const nearDuplicate = this.record.nearDuplicates
+            .filter(nearDuplicate => nearDuplicate.idConditor === response.data.idConditor)
+            .pop();
+          output.similarityRate = nearDuplicate.similarityRate;
+          output.duplicateBySymmetry = nearDuplicate.duplicateBySymmetry;
+          return output;
+        });
         this.sizeColumnHeaderRecord = (this.nearDuplicateRecords.length > 2) ? 3 : Math.floor(12 / (this.nearDuplicateRecords.length + 1));
         this.sizeColumnHeaderNested = (this.nearDuplicateRecords.length > 2) ? 9 : this.sizeColumnHeaderRecord * this.nearDuplicateRecords.length;
         this.sizeColumnHeaderNearDuplicateRecords = (this.nearDuplicateRecords.length > 2) ? 4 : Math.floor(12 / this.nearDuplicateRecords.length);
         this.nearDuplicateRecordSelected = this.nearDuplicateRecords[0];
         this.nearDuplicateRecordSelected.isSelected = true;
-        this.getComparisonInfos();
+        this.recordsComparison = getComparisonInfos(this.record, this.nearDuplicateRecordSelected, CONFIG);
       });
     };
 
@@ -26,82 +36,7 @@ export const recordModal = {
       this.nearDuplicateRecordSelected.isSelected = false;
       nearDuplicateRecord.isSelected = true;
       this.nearDuplicateRecordSelected = nearDuplicateRecord;
-      this.getComparisonInfos();
-    };
-
-    this.getComparisonInfos = function () {
-      const recordsComparison = {};
-      const sortedField = new Set([
-        'source',
-        'title.default',
-        'title.en',
-        'title.fr',
-        'arxiv',
-        'doi',
-        'nnt',
-        'reportNumber',
-        'authorNames',
-        'halAuthorId',
-        'isni',
-        'orcId',
-        'researcherId',
-        'viaf',
-        'publicationDate',
-        'title.journal',
-        'title.monography',
-        'title.meeting',
-        'volume',
-        'xissn',
-        'issn',
-        'eissn',
-        'isbn',
-        'eisbn',
-        'issue',
-        'specialIssue',
-        'pageRange',
-        'articleNumber',
-        'meetAbstrNo',
-        'part',
-        'abstract',
-        'idHal',
-        'pmId',
-        'ppn',
-        'utKey',
-        'documentType',
-        'typeConditor'
-      ]);
-      sortedField.forEach(key => {
-        let record = get(this.record, key, '');
-        let nearDuplicateRecordSelected = get(this.nearDuplicateRecordSelected, key, '');
-        record = (typeof record === 'string') ? record : String(record);
-        nearDuplicateRecordSelected = (typeof nearDuplicateRecord === 'string') ? nearDuplicateRecordSelected : String(nearDuplicateRecordSelected);
-        if ((record.length + nearDuplicateRecordSelected.length) === 0) return;
-        const isEqual = (record.toLowerCase() === nearDuplicateRecordSelected.toLowerCase());
-        const averageNumberCharacters = (record.length + nearDuplicateRecordSelected.length) / 2;
-        const maxSizeAbstract = 3000;
-        const toBeCompared = !['source'].includes(key);
-        const origin = { raw: record };
-        const target = { raw: nearDuplicateRecordSelected };
-        if (averageNumberCharacters < maxSizeAbstract && toBeCompared) {
-          const comparison = diffWords(record, nearDuplicateRecordSelected, { ignoreCase: true });
-          origin.details = comparison.filter(chunk => (!chunk.added));
-          target.details = comparison.filter(chunk => (!chunk.removed));
-        }
-        if (key === 'idHal') {
-          if (record) origin.url = `${CONFIG.hal.baseUrl}/${record}`;
-          if (nearDuplicateRecordSelected) target.url = `${CONFIG.hal.baseUrl}/${nearDuplicateRecordSelected}`;
-        }
-        if (key === 'pmId') {
-          if (record) origin.url = `${CONFIG.pubmed.baseUrl}/${record}`;
-          if (nearDuplicateRecordSelected) target.url = `${CONFIG.pubmed.baseUrl}/${nearDuplicateRecordSelected}`;
-        }
-        if (key === 'orcId') {
-          if (record) origin.url = `${CONFIG.orcid.baseUrl}/${record}`;
-          if (nearDuplicateRecordSelected) target.url = `${CONFIG.orcid.baseUrl}/${nearDuplicateRecordSelected}`;
-        }
-        recordsComparison[key] = [isEqual, origin, target];
-      });
-      this.recordsComparison = recordsComparison;
+      this.recordsComparison = getComparisonInfos(this.record, this.nearDuplicateRecordSelected, CONFIG);
     };
 
     this.checkNearDuplicateRecordsToValidate = function () {
@@ -151,3 +86,85 @@ export const recordModal = {
   },
   template
 };
+
+function getComparisonInfos (record, nearDuplicateRecordSelected, CONFIG) {
+  const recordsComparison = {};
+  const sortedField = new Set([
+    'source',
+    'title.default',
+    'title.en',
+    'title.fr',
+    'arxiv',
+    'doi',
+    'nnt',
+    'reportNumber',
+    'authorNames',
+    'halAuthorId',
+    'isni',
+    'orcId',
+    'researcherId',
+    'viaf',
+    'publicationDate',
+    'title.journal',
+    'title.monography',
+    'title.meeting',
+    'volume',
+    'xissn',
+    'issn',
+    'eissn',
+    'isbn',
+    'eisbn',
+    'issue',
+    'specialIssue',
+    'pageRange',
+    'articleNumber',
+    'meetAbstrNo',
+    'part',
+    'abstract',
+    'idHal',
+    'pmId',
+    'ppn',
+    'utKey',
+    'documentType',
+    'typeConditor'
+  ]);
+  sortedField.forEach(key => {
+    let recordData = get(record, key, '');
+    let nearDuplicateRecordData = get(nearDuplicateRecordSelected, key, '');
+    recordData = (typeof recordData === 'string') ? recordData : String(recordData);
+    nearDuplicateRecordData = (typeof nearDuplicateRecord === 'string') ? nearDuplicateRecordData : String(nearDuplicateRecordData);
+    if ((recordData.length + nearDuplicateRecordData.length) === 0) return;
+    const isEqual = (recordData.toLowerCase() === nearDuplicateRecordData.toLowerCase());
+    const averageNumberCharacters = (recordData.length + nearDuplicateRecordData.length) / 2;
+    const maxSizeAbstract = 3000;
+    const toBeCompared = !['source'].includes(key);
+    const origin = { raw: recordData };
+    const target = { raw: nearDuplicateRecordData };
+    if (averageNumberCharacters < maxSizeAbstract && toBeCompared) {
+      const comparison = diffWords(recordData, nearDuplicateRecordData, { ignoreCase: true });
+      origin.details = comparison.filter(chunk => (!chunk.added));
+      target.details = comparison.filter(chunk => (!chunk.removed));
+    }
+    if (key === 'idHal') {
+      if (recordData) origin.url = `${CONFIG.hal.baseUrl}/${recordData}`;
+      if (nearDuplicateRecordData) target.url = `${CONFIG.hal.baseUrl}/${nearDuplicateRecordData}`;
+    }
+    if (key === 'pmId') {
+      if (recordData) origin.url = `${CONFIG.pubmed.baseUrl}/${recordData}`;
+      if (nearDuplicateRecordData) target.url = `${CONFIG.pubmed.baseUrl}/${nearDuplicateRecordData}`;
+    }
+    if (key === 'orcId') {
+      if (recordData) origin.url = `${CONFIG.orcid.baseUrl}/${recordData}`;
+      if (nearDuplicateRecordData) target.url = `${CONFIG.orcid.baseUrl}/${nearDuplicateRecordData}`;
+    }
+    recordsComparison[key] = [isEqual, origin, target];
+  });
+  return recordsComparison;
+}
+
+function getNearDuplicates (record, conditorApiService) {
+  const nearDuplicateRecords = record.nearDuplicates.map(nearDuplicateRecord => {
+    return conditorApiService.getRecordById(nearDuplicateRecord.idConditor);
+  });
+  return Promise.all(nearDuplicateRecords);
+}
