cd35f6d57b231fc4d7dd82cc5de4827599c3f7cc
Rémy MEJA <remymeja@free.fr>
2019-05-27 11:12:48 +0200

    add better notification message

--- src/components/cornelius/record-list/record-modal/confirm-modal/confirm-modal.component.js
+++ src/components/cornelius/record-list/record-modal/confirm-modal/confirm-modal.component.js
@@ -15,18 +15,18 @@ export const confirmModal = {
         reportNonDuplicates: this.nearDuplicateRecords.filter(record => !record.isDuplicateByUser).map(record => record.idConditor)
       };
       conditorApiService.postDuplicatesValidation(duplicatesValidation).then(() => {
-        const message = 'Le signalement a bien été enregistré.';
+        const message = `Le signalement de la notice (${this.record.sourceId}) a bien été enregistré.`;
         notificationLogService.add(message, 'success');
         $rootScope.$emit('refresh');
       }).catch(response => {
         if (response.data.hasOwnProperty('errors')) {
           response.data.errors.map(error => {
-            const message = `Le traitement de votre signalement n'a pas pu aboutir : ${error.statusName}, ${error.message} (${error.details})`;
+            const message = `Le traitement du signalement de la notice (${this.record.sourceId}) n'a pas pu aboutir : ${error.statusName}, ${error.message} (${error.details})`;
             console.error(error);
             notificationLogService.add(message, 'error');
           });
         } else {
-          const message = `Le traitement de votre signalement n'a pas pu aboutir : ${response.data}`;
+          const message = `Le traitement du signalement de la notice (${this.record.sourceId}) n'a pas pu aboutir : ${response.data}`;
           console.error(response);
           notificationLogService.add(message, 'error');
         }
