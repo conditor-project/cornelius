17a5dcf5ebb6918f30730857f50c12fd53998a7b
Rémy MEJA <remymeja@free.fr>
2019-06-14 07:51:32 +0200

    add alert message for display size requirement

--- src/components/cornelius/cornelius.component.js
+++ src/components/cornelius/cornelius.component.js
@@ -3,9 +3,15 @@ import template from './cornelius.template.html';
 import pkg from '../../../package.json';
 
 export const cornelius = {
-  controller: function () {
+  controller: function ($window) {
     this.$onInit = function () {
       this.version = pkg.version;
+      this.windowWidth = $window.innerWidth;
+      this.windowHeight = $window.window.innerHeight;
+      $window.onresize = () => {
+        this.windowWidth = $window.innerWidth;
+        this.windowHeight = $window.window.innerHeight;
+      };
     };
   },
   template
